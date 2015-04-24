73b453df6c94c30fe05e7598391b2469bbf86c73
Olexander Kutsenko <okutsenko@grossum.com>
2015-04-24 10:21:19 +0300

    added service teamcity to autostart

--- configs/start.sh
+++ configs/start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#Starting ssh and MySQL services
+#Starting SSH and MySQL services
 service ssh start
 service mysql start
 
-
 #Starting TeamCity Server
-cd /opt/TeamCity/bin/
-./runAll.sh start
+service teamcity start
